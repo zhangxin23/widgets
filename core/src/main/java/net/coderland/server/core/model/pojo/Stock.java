79a9f7b8abffc4f0d8b0ca86f89d89b7501ab9a3
zhangxin <xin_zhang23@126.com>
2015-12-23 17:46:05 +0800

    stock table

---
+++ core/src/main/java/net/coderland/server/core/model/pojo/Stock.java
@@ -0,0 +1,63 @@
+package net.coderland.server.core.model.pojo;
+
+public class Stock {
+    private Integer id;
+
+    private String name;
+
+    private String code;
+
+    private Integer price;
+
+    private Long ctime;
+
+    private String unit;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code == null ? null : code.trim();
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Long getCtime() {
+        return ctime;
+    }
+
+    public void setCtime(Long ctime) {
+        this.ctime = ctime;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit == null ? null : unit.trim();
+    }
+}
