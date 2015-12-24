9d56b9f38d48218842855536aa74e811171fd130
zhangxin5 <zhangxin5@kingsoft.com>
2015-12-24 14:19:46 +0800

    init.sql

--- core/sql/core.sql
+++ core/sql/core.sql
@@ -7,3 +7,12 @@ CREATE TABLE `stock` (
   `unit` tinyint(4) NOT NULL COMMENT '1:人民币，2:美元，3:欧元，4:英镑，5:澳元',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE `stock_code` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) COLLATE utf8_bin NOT NULL,
+  `code` varchar(20) COLLATE utf8_bin NOT NULL,
+  `house` tinyint(4) NOT NULL COMMENT '1：上海，2：深圳',
+  `status` tinyint(4) NOT NULL COMMENT '0：失效，1：有效',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
