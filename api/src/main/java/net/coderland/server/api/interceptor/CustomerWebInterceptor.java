22aa4edc00fcdd88bddaede82de4abf228e013db
zhangxin <xin_zhang23@126.com>
2016-03-10 20:31:07 +0800

    test CustomerWebInterceptor

---
+++ api/src/main/java/net/coderland/server/api/interceptor/CustomerWebInterceptor.java
@@ -0,0 +1,11 @@
+package net.coderland.server.api.interceptor;
+
+import org.springframework.web.servlet.handler.WebRequestHandlerInterceptorAdapter;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
+
+/**
+ * User: zhangxin
+ * Date: 2016-03-10 09:55:00
+ */
+public class CustomerWebInterceptor extends WebContentInterceptor {
+}
